086214dfc67b0caa84e8713adfc18ec0dae0976d
Gilajoanna <gilajoannajohansson@gmail.com>
2022-05-04 21:21:53 +0200

    test post article on submit

--- client/__tests__/writeNewArticle.test.jsx
+++ client/__tests__/writeNewArticle.test.jsx
@@ -1,9 +1,10 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 import { WriteNewArticle } from "../writeNewArticle";
-import { act } from "react-dom/test-utils";
+import { act, Simulate } from "react-dom/test-utils";
+import { AppContext } from "../globals/AppContext";
 
-describe("write new article", () => {
+describe("Write new article", () => {
   it("shows new article form", async () => {
     const element = document.createElement("div");
     await act(async () => ReactDOM.render(<WriteNewArticle />, element));
@@ -11,5 +12,32 @@ describe("write new article", () => {
     expect(
       Array.from(element.querySelectorAll("form label")).map((e) => e.innerHTML)
     ).toEqual(["Title:", "Category:", "Content:", "Author:"]);
+  });
+
+  it("Post article on submit", async () => {
+    const postArticle = jest.fn();
+    const element = document.createElement("div");
+    await act(async () =>
+      ReactDOM.render(
+        <AppContext.Provider value={{ postArticle: postArticle }}>
+          {" "}
+          <WriteNewArticle />{" "}
+        </AppContext.Provider>,
+        element
+      )
+    );
+    Simulate.change(element.querySelector("form input:nth-of-type(1)"), {
+      target: { value: "Test article" },
+    });
+    Simulate.change(element.querySelector("form div:nth-of-type(2) input"), {
+      target: { value: "Test category" },
+    });
+    await act(async () => Simulate.submit(element.querySelector("form")));
+    expect(postArticle).toBeCalledWith({
+      title: "Test article",
+      category: "Test category",
+      content: "",
+      author: "",
+    });
   });
 });
